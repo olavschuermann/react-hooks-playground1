54931362fb9b7331a1c916c613e833b67d8a7d6f
Olav Schuermann <developer@netzpluslogik.de>
2019-11-15 11:02:26 +0100

    building structure of ask questions app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Some header text</h1>
+      <form>
+        <input type="text" name="First question to ask" /><br />
+        <input type="text" name="Second question to ask" /><br />
+        <input type="text" name="Third question to ask" /><br />
+        <input type="submit" value="Absenden" />
+      </form>
     </div>
   );
 }
