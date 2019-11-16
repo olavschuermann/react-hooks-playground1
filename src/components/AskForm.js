2809aa3ed915dd099ceb4d403b0c1596b2dde218
Olav Schuermann <developer@netzpluslogik.de>
2019-11-16 17:17:32 +0100

    adding useState hook for content from first input

--- src/components/AskForm.js
+++ src/components/AskForm.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function AskForm() {
+
+    const {firstInput, setFirstInput} = useState('');
+
     return (
         <div>
             <form>
+                {/* // Inhalt aus erstem Feld abfragen und in useState speichern, Ausgabe in console.log */}
                 <input type="text" name="First question to ask" /><br />
-                <input type="text" name="Second question to ask" /><br />
+                {console.log(firstInput)}
+                <input type="number" name="How old are you?" /><br />
                 <input type="text" name="Third question to ask" /><br />
                 <input type="submit" value="Absenden" />
             </form>
