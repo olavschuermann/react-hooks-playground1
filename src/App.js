498cd5e70659489fde409c41d7b9a1e2edd4b542
Olav Schuermann <developer@netzpluslogik.de>
2019-11-16 17:30:54 +0100

    adding ausgabe field, getting input from child component

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ function App() {
   return (
     <div className="App">
       <h1>Some header text</h1>
+      <br /><br />
+      Ausgabe:<br />
+      <input type="text" placeholder="" />
+      <br /><br /><br />
       <AskForm />
     </div>
   );
 }
+
+// Text aus Child Component > Input Field holen und in Ausgabe-Feld anzeigen
 
 export default App;
